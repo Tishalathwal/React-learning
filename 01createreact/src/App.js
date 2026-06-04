9c0ff2f138ea4a5d4b6e37596dabe04e3ee8edb3
Tishalathwal <tishalathwal55@gmail.com>
2026-06-04 23:46:15 +0530

    basic react

---
+++ 01createreact/src/App.js
@@ -0,0 +1,14 @@
+
+import New from "./new";
+
+function App() {
+  return (
+    <>
+    <h1>Hello React</h1>
+    <New />
+    </>
+    
+  );
+}
+
+export default App;
---
+++ 01createreact/src/new.js
@@ -0,0 +1,7 @@
+function New(){
+    return(
+        <h2>CreateReact</h2>
+    )
+}
+
+export default New
